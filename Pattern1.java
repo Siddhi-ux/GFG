ff75f094a435a0f99ce078ac1b0bdcd246bfea01
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-25 15:08:40 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,28 @@
+Pattern - 
+  * * * *
+  * * * *
+  * * * *
+  * * * *
+
+  Solution-
+  class Solution {
+
+    static void printSquare(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+        
+       System.out.println();
+    }
+    }
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printSquare(n);
+      
+    }
+} 
+
