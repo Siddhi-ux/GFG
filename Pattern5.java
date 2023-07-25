f7d4a6c5762b90a727cd9c2809328945fabe6630
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-25 16:06:38 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    static void printTriangle(int n) {
+       for(int i=1;i<=n;i++){
+           for(int j=i;j<=n;j++){
+               System.out.print("* ");
+           }
+          System.out.println();
+       }
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        printTriangle(n);
+    }
+}
