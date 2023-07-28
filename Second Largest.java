69a0da684c7fa4d02ce6d6aad66748709e19cd7f
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-28 18:02:17 +0530

    Create Second Largest.java

---
+++ Second Largest.java
@@ -0,0 +1,25 @@
+class Solution {
+    int print2largest(int arr[], int n) {
+        int first = Integer.MIN_VALUE;
+        int sec =Integer.MIN_VALUE;
+        if(n<2)
+        return -1;
+       
+        for(int num : arr){
+            if(num> first){
+                sec = first;
+                first = num;
+            }else if(num> sec && num !=first){
+                sec =num;
+               
+            }
+           
+        }
+        if(sec == Integer.MIN_VALUE){
+            return -1;
+        }
+       
+        return sec;
+    }
+    
+}
