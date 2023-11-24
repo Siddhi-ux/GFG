59d6a71bc511cc446267498b9e086bc0bfcb8ad4
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-11-24 22:53:44 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,10 @@
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here
+        
+    }
+}
